b75f3dada385cbde6c485a9cf19c135894d3ee7d
Billy Siri <wdsiri@quinnipiac.edu>
2023-04-19 22:07:05 -0400

    Implemented Home Fragment

--- app/src/main/java/edu/quinnipiac/ser210/witswarzone/HomeFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/witswarzone/HomeFragment.kt
@@ -6,17 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.util.Log
 import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
+    lateinit var editText: EditText
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        editText = view.findViewById(R.id.usernameInput)
+        var username = "Guest"
 
-        val button = view.findViewById<Button>(R.id.button_go)
-        button.setOnClickListener{
+        val buttonGo = view.findViewById<Button>(R.id.button_go)
+        buttonGo.setOnClickListener{
+            if(editText.text != null) {
+                username = editText.text.toString()
+            }
+            //Log.d("TAG", username)
+            val action = HomeFragmentDirections.actionHomeFragmentToListFragment()
+            this.findNavController().navigate(action)
+        }
+        val buttonGuest = view.findViewById<Button>(R.id.btn_guest)
+        buttonGuest.setOnClickListener{
+            //Log.d("TAG", username)
             val action = HomeFragmentDirections.actionHomeFragmentToListFragment()
             this.findNavController().navigate(action)
         }

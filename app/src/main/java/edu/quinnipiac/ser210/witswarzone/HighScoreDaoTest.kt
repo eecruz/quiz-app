0b9096470f4db45088c5b7b9b6e0ce8abd00a8c7
Billy Siri <wdsiri@quinnipiac.edu>
2023-04-28 14:50:44 -0400

    Implemented Dao test

---
+++ app/src/main/java/edu/quinnipiac/ser210/witswarzone/HighScoreDaoTest.kt
@@ -0,0 +1,68 @@
+package edu.quinnipiac.ser210.witswarzone
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import edu.quinnipiac.ser210.witswarzone.HighScore
+import org.junit.runner.RunWith
+
+import edu.quinnipiac.ser210.witswarzone.HighScoreDao
+import edu.quinnipiac.ser210.witswarzone.HighScoreRoomDatabase
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class HighScoreDaoTest {
+
+    private lateinit var highScoreDao: HighScoreDao
+    private lateinit var highScoreRoomDatabase: HighScoreRoomDatabase
+    private var highScore1 = HighScore(1,"Guest1",7)
+    private var highscore2 = HighScore(2,"Guest2",5)
+
+    @Before
+    fun createDB() {
+        val context: Context = ApplicationProvider.getApplicationContext()
+
+        highScoreRoomDatabase = Room.inMemoryDatabaseBuilder(context, HighScoreRoomDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        highScoreDao = highScoreRoomDatabase.highScoreDao()
+    }
+
+    private suspend fun addOneHighScoreToDb() {
+        highScoreDao.insert(highScore1)
+    }
+    private suspend fun addTwoHighScoresToDb() {
+        highScoreDao.insert(highScore1)
+        highScoreDao.insert(highscore2)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoInsert_insertsHighScoreIntoDB() = runBlocking {
+        addOneHighScoreToDb()
+        val allHighScores = highScoreDao.getAllScores().first()
+        assertEquals(allHighScores, highScore1)
+    }
+    @Test
+    @Throws(Exception::class)
+    fun daoGetAllHighScores_returnsAllHighScoresFromDB() = runBlocking {
+        addOneHighScoreToDb()
+        val allHighScores = highScoreDao.getAllScores().first()
+        Assert.assertEquals(allHighScores[0],highScore1)
+        Assert.assertEquals(allHighScores[1],highscore2)
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        highScoreRoomDatabase.close()
+    }
+}
